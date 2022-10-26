c219b234151cc3eaa015f2204f7e894b18f8a3ee
Nikita <78175337+pr0100PashaZXC@users.noreply.github.com>
2022-10-26 21:30:20 +0300

    Add files via upload

---
+++ preProject2.1.2/src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String  catString;
+
+    public String getCatString() {
+        return catString;
+    }
+
+    public void setCatString(String catString) {
+        this.catString = catString;
+    }
+}
